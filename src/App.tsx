34eb9e8a7c39d04f065c0669e8f6878de241c52c
tanassar-hussain <tanassarhussain@outlook.com>
2023-11-13 01:09:12 +0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
+import Header from './components/Header';
+import SignIn from './pages/SignIn';
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="about" element={<About/>}/>
+    <Route path="sign-in" element={<SignIn/>}/>
     </Routes>
     </BrowserRouter>
   )
