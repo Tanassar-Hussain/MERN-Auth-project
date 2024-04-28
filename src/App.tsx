2f1c990cc1fb3e6c616e139ec264709d116bdd01
Tanassar Hussain <trhussain@cinnova.com>
2024-04-29 00:01:51 +0500

    add profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './pages/About';
-import Home from './pages/Home';
-import Header from './components/Header';
-import SignIn from './pages/SignIn';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 function App() {
   return (
     <BrowserRouter>
-    {/* header add in all page */}
-    <Header/> 
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="about" element={<About/>}/>
-    <Route path="sign-in" element={<SignIn/>}/>
-    </Routes>
+      {/* header add in all page */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
